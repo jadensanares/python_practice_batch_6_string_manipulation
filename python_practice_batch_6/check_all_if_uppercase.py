ee9cfead4d13f28e604c32dfdde1a3cf2ffa99f2
jadensanares <sanaresj@gmail.com>
2025-03-27 17:29:51 +0800

    add the user input prompt and defined  'all_upper' with the function True

--- python_practice_batch_6/check_all_if_uppercase.py
+++ python_practice_batch_6/check_all_if_uppercase.py
@@ -1,6 +1,12 @@
 # Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 
 # Ask the user to enter a string
+user_input = input("Enter a string: ")
+
 # String that is all uppercase "all_upper = True"
+all_upper = True
+
 # Loop for the characters in the string
+
+
 # Print result
