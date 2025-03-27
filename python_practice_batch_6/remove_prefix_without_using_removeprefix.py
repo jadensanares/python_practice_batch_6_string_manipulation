35683495a8b7733534a9dc0fb78c9e0d7033c2b7
jadensanares <sanaresj@gmail.com>
2025-03-27 17:01:16 +0800

    added the conditions for removing a prefix

--- python_practice_batch_6/remove_prefix_without_using_removeprefix.py
+++ python_practice_batch_6/remove_prefix_without_using_removeprefix.py
@@ -3,12 +3,14 @@
 
 # Ask the user for input
 user_input = input("Enter a string: ")
-prefix = input("Enter prefix to remove: ")
 
 # Ask the user to enter a prefix to remove
-
+prefix = input("Enter prefix to remove: ")
 
 # Make a condition for if the string starts with or without a prefix
-
+if user_input[:len(prefix)] == prefix:
+    cut_string = user_input[len(prefix):]
+else:
+    cut_string = user_input
 
 # Print Result
