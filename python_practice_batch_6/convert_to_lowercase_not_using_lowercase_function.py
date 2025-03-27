3d9996e72820d17b7299f60a32c1f6787035a464
jadensanares <sanaresj@gmail.com>
2025-03-27 17:08:33 +0800

    Pseudocode added for the program

---
+++ python_practice_batch_6/convert_to_lowercase_not_using_lowercase_function.py
@@ -0,0 +1,6 @@
+# Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+# Ask the user to enter a string
+# Make an empty string to store lowercase outcome
+# Make a loop for each character in the string
+# Print result
