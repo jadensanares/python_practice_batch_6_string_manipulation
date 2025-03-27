b0013c2e9448e526d8e8642a60ef4d77d0502e0f
jadensanares <sanaresj@gmail.com>
2025-03-27 18:42:30 +0800

    added the user input prompt and getting the end_length

--- python_practice_batch_6/check_string_ends_with_without_using_endswith_function.py
+++ python_practice_batch_6/check_string_ends_with_without_using_endswith_function.py
@@ -1,7 +1,15 @@
 # Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 
 # Ask the user to input a string
+main_string = input("Enter the main string: ")
+
 # Ask the user to input the ending substring
+ending_substring = input("Enter the ending substring: ")
+
 # Length of the ending substring
+end_length = len(ending_substring)
+
 # Taking the final segment of the main string that is the same length as the ending substring
+
+
 # Comparison of the final segment with the ending substring and print result
