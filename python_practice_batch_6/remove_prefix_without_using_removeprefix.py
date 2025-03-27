5e2e7d235c6a9cd1cd18ea8442779b89005c9080
jadensanares <sanaresj@gmail.com>
2025-03-27 16:56:17 +0800

    pseudocode added for the program

---
+++ python_practice_batch_6/remove_prefix_without_using_removeprefix.py
@@ -0,0 +1,7 @@
+# Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter.
+# Create a program that do the same functionality without using removeprefix() function.
+
+# Ask the user for input
+# Ask the user to enter a prefix to remove
+# Make a condition for if the string starts with or without a prefix
+# Print Result
