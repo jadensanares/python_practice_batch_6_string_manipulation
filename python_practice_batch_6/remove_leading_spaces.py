1916c7a8bbcb9df924174f6af91abc2462677e14
jadensanares <sanaresj@gmail.com>
2025-03-27 12:11:54 +0800

    Psaeudocode added for prog01 of batch 6

---
+++ python_practice_batch_6/remove_leading_spaces.py
@@ -0,0 +1,6 @@
+# Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+
+# Tell the user to enter a string with leading spaces
+# Make an index of the first non-space character
+# Cut the string from the non-space character
+# print result
