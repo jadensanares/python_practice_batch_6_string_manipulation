0a598b0151f96a05e7b3a716b624e90b7c780b8e
jadensanares <sanaresj@gmail.com>
2025-03-27 17:10:21 +0800

    added the user input prompt and empty string to store the lowercase outcome

--- python_practice_batch_6/convert_to_lowercase_not_using_lowercase_function.py
+++ python_practice_batch_6/convert_to_lowercase_not_using_lowercase_function.py
@@ -1,6 +1,12 @@
 # Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 
 # Ask the user to enter a string
+user_input = input("Enter a string: ")
+
 # Make an empty string to store lowercase outcome
+lowercase_string = ""
+
 # Make a loop for each character in the string
+
+
 # Print result
