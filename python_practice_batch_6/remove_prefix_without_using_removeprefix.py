4af4bb4b994d24fab613beec8a3501983751c59e
jadensanares <sanaresj@gmail.com>
2025-03-27 16:57:56 +0800

    added the user prompt for the program

--- python_practice_batch_6/remove_prefix_without_using_removeprefix.py
+++ python_practice_batch_6/remove_prefix_without_using_removeprefix.py
@@ -2,6 +2,13 @@
 # Create a program that do the same functionality without using removeprefix() function.
 
 # Ask the user for input
+user_input = input("Enter a string: ")
+prefix = input("Enter prefix to remove: ")
+
 # Ask the user to enter a prefix to remove
+
+
 # Make a condition for if the string starts with or without a prefix
+
+
 # Print Result
