1b0f602db2092544ec9d04cbf5d7ea6bad436865
jadensanares <sanaresj@gmail.com>
2025-03-27 12:13:43 +0800

    added the user input prompt

--- python_practice_batch_6/remove_leading_spaces.py
+++ python_practice_batch_6/remove_leading_spaces.py
@@ -1,6 +1,12 @@
 # Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 
 # Tell the user to enter a string with leading spaces
+user_input = input("Enter a string with leading spaces: ")
+
 # Make an index of the first non-space character
+
+
 # Cut the string from the non-space character
+
+
 # print result
