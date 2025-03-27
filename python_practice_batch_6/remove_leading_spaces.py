69f4f30e7bff70521906a21d96af98cfa0ca8d59
jadensanares <sanaresj@gmail.com>
2025-03-27 12:16:59 +0800

    fixed the typo error for 'len' previously 'leb'

--- python_practice_batch_6/remove_leading_spaces.py
+++ python_practice_batch_6/remove_leading_spaces.py
@@ -5,7 +5,7 @@ user_input = input("Enter a string with leading spaces: ")
 
 # Make an index of the first non-space character
 index = 0
-while index < leb(user_input) and user_input[index] == " ":
+while index < len(user_input) and user_input[index] == " ":
     index += 1
 
 # Cut the string from the non-space character
