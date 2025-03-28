bea2af9f7e60930c99426f87be80e63618bce74d
jadensanares <sanaresj@gmail.com>
2025-03-28 21:35:22 +0800

    pseudocode added for the program

---
+++ python_practice_batch_6/left_justify_string_without_using_ljust.py
@@ -0,0 +1,7 @@
+# Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+# Ask the user to enter the input (string)
+# Ask the user to enter the width for spaces
+# total number of spaces
+# condition for adding of spaces
+# print result
