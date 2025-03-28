c0930aec7a2eb290d912254b5515c8b5a6116e28
jadensanares <sanaresj@gmail.com>
2025-03-28 23:15:42 +0800

    pseudocode added

---
+++ python_practice_batch_6/title_case_string_without_using_titlecase_function.py
@@ -0,0 +1,6 @@
+# Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+
+# prompt user input (string)
+# splitting of words
+# checking conditions of the word
+# print result
