023dde85216828fc023d663131377131348093e8
jadensanares <sanaresj@gmail.com>
2025-03-28 23:17:35 +0800

    added the prompt user input and the defined the words as it splits it

--- python_practice_batch_6/title_case_string_without_using_titlecase_function.py
+++ python_practice_batch_6/title_case_string_without_using_titlecase_function.py
@@ -1,6 +1,12 @@
 # Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 
 # prompt user input (string)
+input_string = input("Enter the string: ")
+
 # splitting of words
+words = input_string.split()
+
 # checking conditions of the word
+
+
 # print result
