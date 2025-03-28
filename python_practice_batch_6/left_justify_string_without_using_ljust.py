ee850f3fee00a5ff46e413b60134e6d44012bcb5
jadensanares <sanaresj@gmail.com>
2025-03-28 21:39:33 +0800

    added the prompt user input and changed some pseudocodes

--- python_practice_batch_6/left_justify_string_without_using_ljust.py
+++ python_practice_batch_6/left_justify_string_without_using_ljust.py
@@ -1,7 +1,12 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 
 # Ask the user to enter the input (string)
+text_string = input("Enter the string: ")
+
 # Ask the user to enter the width for spaces
-# total number of spaces
+total_width = int(input("Enter the total width: "))
+
 # condition for adding of spaces
+
+
 # print result
