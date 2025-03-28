765799939fd7ec9a5ad66ac8301b5eb9e7f292e8
jadensanares <sanaresj@gmail.com>
2025-03-28 23:06:28 +0800

    added the conditions for making the first letter of the string capitalized

--- python_practice_batch_6/capitalize_string_without_using_capitalize_function.py
+++ python_practice_batch_6/capitalize_string_without_using_capitalize_function.py
@@ -1,5 +1,12 @@
 # Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 
 # prompt user input
+input_string = input("Enter the string: ")
+
 # capitalization of first letter of the string
+if input_string:
+    capitalized_string = input_string[0].upper() + input_string[1:].lower()
+else:
+    capitalized_string = ""
+
 # print result
