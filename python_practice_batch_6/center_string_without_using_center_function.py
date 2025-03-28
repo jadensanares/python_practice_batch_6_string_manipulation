8873f544a11cfdbfbc7a2de56ec52cbde5ed8c1e
jadensanares <sanaresj@gmail.com>
2025-03-28 21:51:43 +0800

    added the prompt user input for the string and total width of spaces to make it center

--- python_practice_batch_6/center_string_without_using_center_function.py
+++ python_practice_batch_6/center_string_without_using_center_function.py
@@ -1,6 +1,11 @@
 # Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 
 # Get user input for the string
+text_string = input("Enter the string: ")
+total_width = int(input("Enter the total width: "))
+
 # Computation of spaces
+
+
 # print result
 
