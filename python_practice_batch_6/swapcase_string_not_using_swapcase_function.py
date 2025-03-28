5df2af6e9fadaec628f297e89991e4db24ac96b7
jadensanares <sanaresj@gmail.com>
2025-03-28 22:52:26 +0800

    added the prompt user input, added trhe loop for the swap case by using .isupper() and .islower()

--- python_practice_batch_6/swapcase_string_not_using_swapcase_function.py
+++ python_practice_batch_6/swapcase_string_not_using_swapcase_function.py
@@ -1,5 +1,16 @@
 # Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 
 # prompt user input
+input_string = input("Enter the string: ")
+
 # using simple loop for swap case without using swapcase() function
+swapcase_string = ""
+for character in input_string:
+    if character.isupper():
+        swapcase_string += character.lower()
+    elif character.islower():
+        swapcase_string += character.upper()
+    else:
+        swapcase_string += character
+
 # print result
