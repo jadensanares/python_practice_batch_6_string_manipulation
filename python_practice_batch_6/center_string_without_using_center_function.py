47ee996d75231117e43dfcdba3569e38b9e73956
jadensanares <sanaresj@gmail.com>
2025-03-28 21:50:05 +0800

    pseudocode added for the program

---
+++ python_practice_batch_6/center_string_without_using_center_function.py
@@ -0,0 +1,6 @@
+# Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+# Get user input for the string
+# Computation of spaces
+# print result
+
