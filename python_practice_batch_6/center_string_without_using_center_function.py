d5617f0ddfeee5fc2ebd5e4f7ff497a584399132
jadensanares <sanaresj@gmail.com>
2025-03-28 21:55:45 +0800

    added the computation for the spaces and the print output, working

--- python_practice_batch_6/center_string_without_using_center_function.py
+++ python_practice_batch_6/center_string_without_using_center_function.py
@@ -3,9 +3,10 @@
 # Get user input for the string
 text_string = input("Enter the string: ")
 total_width = int(input("Enter the total width: "))
-
-# Computation of spaces
-
+  
+# Computation of spaces.
+left_spaces = (total_width - len(text_string)) // 2
+right_spaces = total_width - len(text_string) - left_spaces
 
 # print result
-
+print(f"'{ ' ' * left_spaces + text_string + ' ' * right_spaces }'")
