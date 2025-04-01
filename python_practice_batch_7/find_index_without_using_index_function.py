764098ef4ceaeb804f3cc75c30d1364a4e8b1512
jadensanares <sanaresj@gmail.com>
2025-04-01 17:50:54 +0800

    pseudocode added

---
+++ python_practice_batch_7/find_index_without_using_index_function.py
@@ -0,0 +1,6 @@
+# Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+
+# prompt user for the input string and substring
+# start the index variable
+# start the loop for finding the first occurence of said substring
+# print result
