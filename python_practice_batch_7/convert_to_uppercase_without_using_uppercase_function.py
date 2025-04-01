20b1c326454d641775daba01fa0c61a011688b20
jadensanares <sanaresj@gmail.com>
2025-04-01 15:22:52 +0800

    fixed the syntax error by removed an additional ')'

--- python_practice_batch_7/convert_to_uppercase_without_using_uppercase_function.py
+++ python_practice_batch_7/convert_to_uppercase_without_using_uppercase_function.py
@@ -1,13 +1,13 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
 # Prompt user to input string
-string_input = input("Enter the string :")
+string_input = input("Enter the string: ")
 
 # Condition loop for converting the lowercase letters to uppercase letter without using the upper() function
 uppercase_string = ""
 for character in string_input:
     if 'a' <= character <= 'z':
-        uppercase_string += chr(ord(character)) - 32)
+        uppercase_string += chr(ord(character) - 32)
     else:
         uppercase_string += character
 
