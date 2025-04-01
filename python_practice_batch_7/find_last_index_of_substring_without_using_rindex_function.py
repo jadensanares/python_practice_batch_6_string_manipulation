5e4b6e942ca1abe5670e691695b7ba5266e5fb09
jadensanares <sanaresj@gmail.com>
2025-04-01 18:20:38 +0800

    added the prompt user input and defined the variablke for index of substring

--- python_practice_batch_7/find_last_index_of_substring_without_using_rindex_function.py
+++ python_practice_batch_7/find_last_index_of_substring_without_using_rindex_function.py
@@ -1,6 +1,13 @@
 # Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 
 # prompt user for input of string and substring
+string_input = input("Enter the string: ")
+substring = input("Enter the subtring you want to find from the last position: ")
+
 # start the index variable
+index_of_substring = -1
+
 # loop string but in reverse 
+
+
 # print result
