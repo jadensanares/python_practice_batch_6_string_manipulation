f397a2a7ec43ece68a331e6d6b5c7cf1630115cc
jadensanares <sanaresj@gmail.com>
2025-04-01 16:44:33 +0800

    pseudocode added

---
+++ python_practice_batch_7/zero_fill_string_without_using_zerofill_function.py
@@ -0,0 +1,6 @@
+# Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+
+# Prompt the user to input string
+# calculation of the number of zeros 
+# defining the string with zeros filled
+# print result
