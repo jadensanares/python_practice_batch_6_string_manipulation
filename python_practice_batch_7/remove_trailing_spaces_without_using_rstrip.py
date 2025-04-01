8501f163e92c160ec7df7eb58552c5d3f95d7b76
jadensanares <sanaresj@gmail.com>
2025-04-01 13:44:29 +0800

    added the conditions for removing the trailing spaces in the string

--- python_practice_batch_7/remove_trailing_spaces_without_using_rstrip.py
+++ python_practice_batch_7/remove_trailing_spaces_without_using_rstrip.py
@@ -4,6 +4,8 @@
 string_input = input("Enter a string with trailing spaces: ")
 
 # remmove trailing spaces without using rstrio() function
-
+last_char_index = len(string_input) -  1
+while last_char_index >= 0 and string_input[last_char_index] == " ":
+    last_char_index -= 1
 
 # print result
