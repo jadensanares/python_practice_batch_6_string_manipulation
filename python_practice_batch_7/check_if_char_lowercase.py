db9eba50d034bd594186bb849dd94ea80888a925
jadensanares <sanaresj@gmail.com>
2025-04-01 15:31:42 +0800

    pseudocode added

---
+++ python_practice_batch_7/check_if_char_lowercase.py
@@ -0,0 +1,5 @@
+# Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+
+# prompt user to input string
+# checking if all characters of the string is on lower case
+# print the result
