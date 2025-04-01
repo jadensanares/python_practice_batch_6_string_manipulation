361979baad9740b22422c5ac2b282e5ab52b28fa
jadensanares <sanaresj@gmail.com>
2025-04-01 16:16:07 +0800

    pseudocode added

---
+++ python_practice_batch_7/right_justify_string_without_using_rightjustify_function..py
@@ -0,0 +1,6 @@
+# Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+
+# prompt user for the string input
+# spaces needed for the letters to go to the right
+# defining the right justified string
+# print result
