290e074b553ccba14d9f32230077ae7502f39073
jadensanares <sanaresj@gmail.com>
2025-04-01 16:22:04 +0800

    added the prompt user to input the string and defined the width

--- python_practice_batch_7/right_justify_string_without_using_rightjustify_function..py
+++ python_practice_batch_7/right_justify_string_without_using_rightjustify_function..py
@@ -1,6 +1,13 @@
 # Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 
 # prompt user for the string input
+string_input = input("Enter the string: ")
+width = int(input("Enter the width: "))
+
 # spaces needed for the letters to go to the right
+
+
 # defining the right justified string
+
+
 # print result
