a8f13d4011cbd037cef8e0dcb037d634858401e2
jadensanares <sanaresj@gmail.com>
2025-04-01 15:50:13 +0800

    testing the code, working

--- python_practice_batch_7/check_if_string_starts_with_not_using_startswith_function.py
+++ python_practice_batch_7/check_if_string_starts_with_not_using_startswith_function.py
@@ -5,6 +5,10 @@ string_input = input("Enter the string: ")
 prefix = input("Enter the prefix: ")
 
 # check the string if it starts with the given prefix by the user
-
+is_prefix = string_input[:len(prefix)] == prefix
 
 # print result
+if is_prefix:
+    print("The string starts with the prefix:", prefix)
+else:
+    print("The string does not start with", prefix)
