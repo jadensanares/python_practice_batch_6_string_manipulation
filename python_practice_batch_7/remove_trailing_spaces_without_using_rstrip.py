31a36103847ebec44d494966702a0b0a8ca912fc
jadensanares <sanaresj@gmail.com>
2025-04-01 14:35:51 +0800

    testing of the program if it's working, an index error occured

--- python_practice_batch_7/remove_trailing_spaces_without_using_rstrip.py
+++ python_practice_batch_7/remove_trailing_spaces_without_using_rstrip.py
@@ -3,9 +3,13 @@
 # Prompt the user for the input
 string_input = input("Enter a string with trailing spaces: ")
 
-# remmove trailing spaces without using rstrio() function
+# remove trailing spaces without using rstrip() function
 last_char_index = len(string_input) -  1
 while last_char_index >= 0 and string_input[last_char_index] == " ":
     last_char_index -= 1
 
+# Trimming the string 
+trimmed_string = string_input[last_char_index + 1]
+
 # print result
+print(f"Trimmed string: '{trimmed_string}'")
