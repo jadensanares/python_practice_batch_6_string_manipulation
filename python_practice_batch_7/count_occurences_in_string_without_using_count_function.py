f8036c157c07be47f3b38604ef7ca8ecf09136d2
jadensanares <sanaresj@gmail.com>
2025-04-01 17:09:20 +0800

    added the user prompt input and started the count for occurences = '0'

--- python_practice_batch_7/count_occurences_in_string_without_using_count_function.py
+++ python_practice_batch_7/count_occurences_in_string_without_using_count_function.py
@@ -1,6 +1,13 @@
 # Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 
 # prompt the user to input string
+string_input = input("Enter the string: ")
+substring = input("Enter the substring for the count: ")
+
 # start a counter for the occurences
+count = 0
+
 # make the loop for the string
+
+
 # print result
