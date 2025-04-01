64e34b9a341f657c1b441ae1553a15e698c164bd
jadensanares <sanaresj@gmail.com>
2025-04-01 17:56:34 +0800

    added the prompt user input and defined the variable name for 'index_of_substring'

--- python_practice_batch_7/find_index_without_using_index_function.py
+++ python_practice_batch_7/find_index_without_using_index_function.py
@@ -1,6 +1,14 @@
 # Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 
 # prompt user for the input string and substring
+string_input = input("Enter the string: ")
+substring = input("Enter the substring you want to find in the string: ")
+
 # start the index variable
+index_of_substring = -1
+
 # start the loop for finding the first occurence of said substring
+
+
 # print result
+
