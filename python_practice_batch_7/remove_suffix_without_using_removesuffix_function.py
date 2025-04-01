96ec7e19ab48d76ee8a554664bd3ed55bf1955ee
jadensanares <sanaresj@gmail.com>
2025-04-01 15:01:04 +0800

    fixed the output problem by adding the '-' before the len, program working

--- python_practice_batch_7/remove_suffix_without_using_removesuffix_function.py
+++ python_practice_batch_7/remove_suffix_without_using_removesuffix_function.py
@@ -6,7 +6,7 @@ suffix = input("Enter the suffix that will be removed: ")
 
 # make the conditions for checking if the strings ends with the provided suffix
 if string_input.endswith(suffix):
-    trimmed_string = string_input[: len(suffix)]
+    trimmed_string = string_input[: -len(suffix)]
 else:
     trimmed_string = string_input
 
