ab55384ffb9559bfb950d90013e86820dfb83b30
jadensanares <sanaresj@gmail.com>
2025-04-01 13:33:27 +0800

    added the user prompt for the user to input a string

--- python_practice_batch_7/remove_trailing_spaces_without_using_rstrip.py
+++ python_practice_batch_7/remove_trailing_spaces_without_using_rstrip.py
@@ -1,5 +1,9 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 
 # Prompt the user for the input
+string_input = input("Enter a string with trailing spaces: ")
+
 # remmove trailing spaces without using rstrio() function
+
+
 # print result
