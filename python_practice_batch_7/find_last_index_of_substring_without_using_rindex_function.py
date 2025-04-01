bd13d84df6f318903800df71cdcbe0817f63bb20
jadensanares <sanaresj@gmail.com>
2025-04-01 18:18:53 +0800

    pseudocode added

---
+++ python_practice_batch_7/find_last_index_of_substring_without_using_rindex_function.py
@@ -0,0 +1,6 @@
+# Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+
+# prompt user for input of string and substring
+# start the index variable
+# loop string but in reverse 
+# print result
