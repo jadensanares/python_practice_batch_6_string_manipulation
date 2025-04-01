d39dcf2e8535aca829107a4fb1a4a4bb3d75c240
jadensanares <sanaresj@gmail.com>
2025-04-01 15:16:58 +0800

    pseudocode added

---
+++ python_practice_batch_7/convert_to_uppercase_without_using_uppercase_function.py
@@ -0,0 +1,5 @@
+# Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+
+# prompt user to input string
+# Condition loop for converting the lowercase letters to uppercase letter without using the upper() function
+# print result
