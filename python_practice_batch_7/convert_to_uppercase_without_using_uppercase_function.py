0368d8da8bda7eb7a00100b98c9c85237e54cd7a
jadensanares <sanaresj@gmail.com>
2025-04-01 15:20:40 +0800

    added the conditions for the converion of all lowercase to uppercase letters

--- python_practice_batch_7/convert_to_uppercase_without_using_uppercase_function.py
+++ python_practice_batch_7/convert_to_uppercase_without_using_uppercase_function.py
@@ -1,5 +1,14 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
-# prompt user to input string
+# Prompt user to input string
+string_input = input("Enter the string :")
+
 # Condition loop for converting the lowercase letters to uppercase letter without using the upper() function
-# print result
+uppercase_string = ""
+for character in string_input:
+    if 'a' <= character <= 'z':
+        uppercase_string += chr(ord(character)) - 32)
+    else:
+        uppercase_string += character
+
+# Print result
