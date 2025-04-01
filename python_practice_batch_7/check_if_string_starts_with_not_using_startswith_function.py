dac7beaa3bbcb588266617cc62cb3c8db4d541d1
jadensanares <sanaresj@gmail.com>
2025-04-01 15:42:48 +0800

    pseudocodea added

---
+++ python_practice_batch_7/check_if_string_starts_with_not_using_startswith_function.py
@@ -0,0 +1,5 @@
+# Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+# prompt user for string input
+# check the string if it starts with the given prefix by the user
+# print result
