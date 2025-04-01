aa4139e0eea6388244bc6ebe82c064db137f894c
jadensanares <sanaresj@gmail.com>
2025-04-01 13:31:24 +0800

    pseudocode added for the program

--- python_practice_batch_7/remove_trailing_spaces_without_using_rstrip.py
+++ python_practice_batch_7/remove_trailing_spaces_without_using_rstrip.py
@@ -1,1 +1,5 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+# Prompt the user for the input
+# remmove trailing spaces without using rstrio() function
+# print result
