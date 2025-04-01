feb2b464fdb5e08418f883dda9dbf95b8655175d
jadensanares <sanaresj@gmail.com>
2025-04-01 14:54:35 +0800

    added the user prompt

--- python_practice_batch_7/remove_suffix_without_using_removesuffix_function.py
+++ python_practice_batch_7/remove_suffix_without_using_removesuffix_function.py
@@ -1,5 +1,10 @@
 # Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 
 # prompt the user for the input
+string_input = input("Enter the string: ")
+suffix = input("Enter the suffix that will be removed: ")
+
 # make the conditions for checking if the strings ends with the provided suffix
+
+
 # print result
