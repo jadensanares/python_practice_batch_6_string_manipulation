9e12789c2892d25c3e7ecd993c1db5073ccdf928
jadensanares <sanaresj@gmail.com>
2025-04-01 15:46:00 +0800

    added the prompt user input

--- python_practice_batch_7/check_if_string_starts_with_not_using_startswith_function.py
+++ python_practice_batch_7/check_if_string_starts_with_not_using_startswith_function.py
@@ -1,5 +1,10 @@
 # Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 
 # prompt user for string input
+string_input = input("Enter the string: ")
+prefix = input("Enter the prefix: ")
+
 # check the string if it starts with the given prefix by the user
+
+
 # print result
