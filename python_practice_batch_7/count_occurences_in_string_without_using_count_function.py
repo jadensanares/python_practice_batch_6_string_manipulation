c644d36f051288f2a7ec43d6c13ac9eec235db0c
jadensanares <sanaresj@gmail.com>
2025-04-01 17:05:45 +0800

    pseudocode added

---
+++ python_practice_batch_7/count_occurences_in_string_without_using_count_function.py
@@ -0,0 +1,6 @@
+# Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+# prompt the user to input string
+# start a counter for the occurences
+# make the loop for the string
+# print result
