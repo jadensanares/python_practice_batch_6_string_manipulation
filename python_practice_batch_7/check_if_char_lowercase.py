45cbd3d2165240da7f731b25baaa0332b2b2daa4
jadensanares <sanaresj@gmail.com>
2025-04-01 15:34:55 +0800

    added the checker for characters if they are all lowercased

--- python_practice_batch_7/check_if_char_lowercase.py
+++ python_practice_batch_7/check_if_char_lowercase.py
@@ -1,5 +1,13 @@
 # Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
 
 # prompt user to input string
+string_input = input("Enter the string: ")
+
 # checking if all characters of the string is on lower case
+all_char_lowercase = True
+
+for character in string_input:
+    if "A" <= character <= "Z":
+        all_char_lowercase = False
+
 # print the result
