f7f091c986e5f3e9a26642e7af9354a437495737
jadensanares <sanaresj@gmail.com>
2025-04-01 16:45:57 +0800

    prompt user added

--- python_practice_batch_7/zero_fill_string_without_using_zerofill_function.py
+++ python_practice_batch_7/zero_fill_string_without_using_zerofill_function.py
@@ -1,6 +1,13 @@
 # Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
 # Prompt the user to input string
+string_input = input("Enter the string: ")
+width = int(input("Enter the width: "))
+
 # calculation of the number of zeros 
+
+
 # defining the string with zeros filled
+
+
 # print result
